6f6985d6599638f9e5c1e746dcb245893b040522
AmeerHamza2348 <ameera2309@outlook.com>
2026-01-18 16:52:52 +0500

    implement data-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-"use client";
-
-import { Suspense } from "react";
-
-import { WelcomePage } from "@refinedev/core";
 import DashboardPage from "@app/dashboard/page";
 
 export default function IndexPage() {
   return (
-    <Suspense>
       <DashboardPage />
-    </Suspense>
   );
 }
